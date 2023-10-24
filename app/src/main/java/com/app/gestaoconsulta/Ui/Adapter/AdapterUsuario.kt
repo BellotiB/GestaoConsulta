8e00602f259d1ba5d7320061ffa3e7e43b96bc9c
Bruno Belloti <92866027+BellotiB@users.noreply.github.com>
2023-10-24 18:10:39 -0300

    feat usuarios fragment

---
+++ app/src/main/java/com/app/gestaoconsulta/Ui/Adapter/AdapterUsuario.kt
@@ -0,0 +1,47 @@
+package com.app.gestaoconsulta.Ui.Adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.app.gestaoconsulta.Model.Usuarios
+import com.app.gestaoconsulta.databinding.ItemUsuariosBinding
+
+
+class AdapterUsuario(private val usuariosList:MutableList<Usuarios>,
+
+):RecyclerView.Adapter<AdapterUsuario.UsuarioViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsuarioViewHolder {
+        val binding = ItemUsuariosBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return UsuarioViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return usuariosList.size
+    }
+
+    override fun onBindViewHolder(holder: UsuarioViewHolder, position:Int) {
+        val usuario = usuariosList[position]
+        holder.bind(usuario)
+    }
+
+    fun updateUsuarioList(user: MutableList<Usuarios>) {
+        usuariosList.clear()
+        usuariosList.addAll(user)
+        notifyDataSetChanged()
+    }
+
+    inner class UsuarioViewHolder(private val binding: ItemUsuariosBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(cadastro: Usuarios) {
+            binding.tvNome.text = cadastro.nome
+            binding.tvEmail.text = cadastro.email
+            binding.tvTelefone.text = cadastro.telefone
+
+        }
+    }
+}
--- app/src/main/java/com/app/gestaoconsulta/Ui/Fragments/UsuariosFragment.kt
+++ app/src/main/java/com/app/gestaoconsulta/Ui/Fragments/UsuariosFragment.kt
@@ -5,23 +5,62 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.app.gestaoconsulta.Model.Usuarios
 import com.app.gestaoconsulta.R
+import com.app.gestaoconsulta.Ui.Adapter.AdapterPedidoAgendamento
+import com.app.gestaoconsulta.Ui.Adapter.AdapterUsuario
+import com.app.gestaoconsulta.ViewModel.ConsultaViewModel
 import com.app.gestaoconsulta.databinding.FragmentMenuBinding
 import com.app.gestaoconsulta.databinding.FragmentUsuariosBinding
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 
 class UsuariosFragment : Fragment() {
 
     private var _binding: FragmentUsuariosBinding? = null
     private val binding get() = _binding!!
+    private val usuarios = mutableListOf<Usuarios>()
+    private var adapter : AdapterUsuario? = null
+
+    private var consultaViewModel : ConsultaViewModel? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         _binding = FragmentUsuariosBinding.inflate(inflater, container, false)
         return binding.root
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        consultaViewModel  = ViewModelProvider(requireActivity())[ConsultaViewModel::class.java]
+        loadUsuarios()
+        setupRecyclerView()
 
-
+    }
+    private fun loadUsuarios() {
+        lifecycleScope.launch {
+            consultaViewModel?.allUsuarios?.collectLatest {users ->
+                users.forEach {
+                    val usuario = Usuarios()
+                    usuario.idUsuario = it.idUsuario
+                    usuario.id = it.id
+                    usuario.nome = it.nome
+                    usuario.email = it.email
+                    usuario.telefone = it.telefone
+                    usuarios.add(usuario)
+                }
+                adapter?.updateUsuarioList(usuarios)
+            }
+        }
+    }
+    private fun setupRecyclerView() {
+        binding.rvDatasList.layoutManager = LinearLayoutManager(requireContext())
+        adapter = AdapterUsuario(mutableListOf())
+        binding.rvDatasList.adapter = adapter
+    }
 }
