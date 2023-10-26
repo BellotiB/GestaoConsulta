4c9ce8dfa2497f3592015cd21158462a37d5d8d9
Bruno Belloti <92866027+BellotiB@users.noreply.github.com>
2023-10-26 12:12:42 -0300

    feat notification push

--- app/src/main/java/com/app/gestaoconsulta/Ui/MainActivity.kt
+++ app/src/main/java/com/app/gestaoconsulta/Ui/MainActivity.kt
@@ -1,8 +1,17 @@
 package com.app.gestaoconsulta.Ui
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import com.app.gestaoconsulta.Model.CadastroMedico
 import com.app.gestaoconsulta.Model.DatasCadastradas
@@ -10,18 +19,55 @@ import com.app.gestaoconsulta.R
 import com.app.gestaoconsulta.ViewModel.ConsultaViewModel
 import com.app.gestaoconsulta.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(),LoadFragment {
 
     private lateinit var binding: ActivityMainBinding
+    private val CHANNEL_ID = "my_channel_id"
 
-     lateinit var consultaViewModel: ConsultaViewModel
+     private lateinit var consultaViewModel: ConsultaViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         consultaViewModel = ViewModelProvider(this)[ConsultaViewModel::class.java]
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelName = "My Channel"
+            val description = "Channel Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+            val channel = NotificationChannel(CHANNEL_ID, channelName, importance).apply {
+                this.description = description
+            }
+
+            val notificationManager = NotificationManagerCompat.from(this)
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun showNotification(title: String, message: String) {
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_save) // Substitua pelo seu ícone de notificação
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        val notificationManager = NotificationManagerCompat.from(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        notificationManager.notify(1, builder.build())
     }
 
     override fun openSecondFragment() {
