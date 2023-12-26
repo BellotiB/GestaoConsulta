ea24ee2cf03f65ff9e5390b362abf8358318b3ca
Bruno Belloti <92866027+BellotiB@users.noreply.github.com>
2023-12-26 15:44:58 -0300

    feat dropdown selecionar medicos

--- app/src/main/java/com/app/gestaoconsulta/Ui/Fragments/CriarAgendFragment.kt
+++ app/src/main/java/com/app/gestaoconsulta/Ui/Fragments/CriarAgendFragment.kt
@@ -11,12 +11,20 @@ import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.app.gestaoconsulta.Model.CadastroMedico
 import com.app.gestaoconsulta.Model.Usuarios
-import com.app.gestaoconsulta.Ui.Adapter.AdapterCadastro
+import com.app.gestaoconsulta.Ui.Adapter.AdapterMedicosCadastrados
 import com.app.gestaoconsulta.Ui.LoadFragment
 import com.app.gestaoconsulta.ViewModel.ConsultaViewModel
 import com.app.gestaoconsulta.databinding.FragmentCriarAgendBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+import java.util.TimeZone
 
 
 class CriarAgendFragment : Fragment() {
@@ -26,6 +34,10 @@ class CriarAgendFragment : Fragment() {
     private var consultaViewModel : ConsultaViewModel? = null
     private var usuarioSelecioando = Usuarios()
     private val cadastrosList = mutableListOf<CadastroMedico>()
+    private var dataAtendimento = ""
+    private var horaAtendimento = ""
+    private var medicoSelecionado = ""
+    private var adapter: AdapterMedicosCadastrados? = null
     private lateinit var callBack : LoadFragment
 
 
@@ -43,7 +55,12 @@ class CriarAgendFragment : Fragment() {
         consultaViewModel  = ViewModelProvider(requireActivity())[ConsultaViewModel::class.java]
         loadUsuarioSelecionado()
         configureView()
+        configureDate()
+        configureHour()
+        loadMedico()
     }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
@@ -64,18 +81,69 @@ class CriarAgendFragment : Fragment() {
     private fun configureView() {
         binding.tvNome.text = usuarioSelecioando.nome
     }
-    private fun updateList(){
-        lifecycleScope.launch {
-            consultaViewModel?.allCadastros?.collect {
-                it.forEach {
-                    val cadastro = CadastroMedico()
-                    cadastro.nome = it.nome
-                    cadastro.especialidade = it.especialidade
-                    cadastro.id = it.id
+    private fun configureDate() {
+        binding.inputData.setOnClickListener {
+            setupDataPicker()
+        }
+    }
+    private fun configureHour() {
+        binding.tvHorario.setOnClickListener {
+            setupHorario()
+        }
+    }
+        private fun loadMedico(){
+            lifecycleScope.launch {
+                consultaViewModel?.allCadastros?.collect {
+                    it.forEach {
+                        val cadastro = CadastroMedico()
+                        cadastro.nome = it.nome
+                        cadastro.especialidade = it.especialidade
+                        cadastro.id = it.id
 
-                    cadastrosList.add(cadastro)
+                        cadastrosList.add(cadastro)
+                    }
+                    configureDropDown()
                 }
             }
+    }
+    private fun configureDropDown() {
+        val nomesMedicos = cadastrosList.map { it.nome }
+        (binding.dropdownMenu.editText as? MaterialAutoCompleteTextView)?.setSimpleItems(nomesMedicos.toTypedArray())
+        medicoSelecionado = binding.dropdownMenu.editText?.text.toString()
+    }
+
+    private fun setupDataPicker() {
+        val dateRangePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Dia do atendimento")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds()).build()
+
+        dateRangePicker.addOnPositiveButtonClickListener { selection ->
+            val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            val startDateCalendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+            startDateCalendar.timeInMillis = selection
+            startDateCalendar.add(Calendar.DAY_OF_MONTH, 1)
+            dataAtendimento = dateFormat.format(startDateCalendar.time)
+
+            binding.inputData.text = dataAtendimento
         }
+
+        dateRangePicker.show(childFragmentManager,"DATA_PICKER")
+    }
+
+    private fun setupHorario(){
+        val picker =
+            MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_12H)
+                .setHour(12)
+                .setMinute(10)
+                .setTitleText("Select Appointment time")
+                .build()
+
+        picker.addOnPositiveButtonClickListener {
+            horaAtendimento = "${picker.hour} : ${picker.minute}"
+            binding.tvHorario.text = horaAtendimento
+        }
+        picker.show(childFragmentManager,"HORA_PICKER")
     }
 }
